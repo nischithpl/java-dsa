5785a0f486ab49d607955ec3d5a8b8976fc732e4
Nischith PL <nischithpl21@gmail.com>
2026-07-27 20:39:27 +0530

    Basic java fundamentals

---
+++ Java-Basics/SumofN.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumofN {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        int i=0;
+        while(i <= n){
+            sum=sum+i;
+            i++;
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
